2bbb30a9118781c8d4dcfefdccb8f7e8eedf5bca
roydh-dn <roydh@dnotitia.com>
2024-10-06 11:30:51 +0900

    chore: modify main to test sift_1b change

--- tests/cpp/main.cpp
+++ tests/cpp/main.cpp
@@ -1,8 +1,21 @@
-
+#include "../../third_party/argparse/argparse.h"
 
 void sift_test1B();
-int main() {
-    sift_test1B();
+void sift_test1B_fp();
+int main(int argc, char *argv[]) {
+    argparse::ArgumentParser parser("sift_1b");
 
+    parser.add_argument("-f", "--float")
+    .help("run sift 1b as float point interface")
+    .default_value(false)
+    .implicit_value(true);
+
+    parser.parse_args(argc, argv);
+
+    if (parser["--float"] == true) {
+        sift_test1B_fp();
+    } else {
+        sift_test1B();
+    }
     return 0;
 }
